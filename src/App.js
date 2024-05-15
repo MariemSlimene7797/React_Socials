9a9fde92270a4cd48c14d869f59021f338a48786
mariem7797 <mariem.slimene7797@gmail.com>
2024-05-15 19:50:09 +0100

    feat(facebook):authentify with facebook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Facebook from './components/Facebook';
+import * as ReactDOM from 'react-dom/client';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You need to authenticate with Facebook to connect.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       <Facebook/>
       </header>
     </div>
   );
